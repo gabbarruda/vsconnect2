2790ab949740a4bf3bd5e2dc4e0b96c87b5bc687
Gabriela Arruda <gabrielacosta460@hotmail.com>
2023-08-08 20:00:23 -0300

    servicos

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom/client'
 //import App from './App'//
 import './index.css'
 import Home from './pages/Home'
+import Servicos from './pages/servicos'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Home/>
+  
+    <Servicos/>
   </React.StrictMode>,
 )
+
